8207a014fa05b61102bc2b57573e1015fb5c94da
Pedram <p.vosooghinia69@gmail.com>
2024-01-30 02:17:41 +0330

    change header

--- src/components/Main/Footer.js
+++ src/components/Main/Footer.js
@@ -1,28 +1,15 @@
 "use client";
 import Link from "next/link";
-import { FaCartShopping } from "react-icons/fa6";
 import { MdOutlinePeopleOutline } from "react-icons/md";
 import { BsTelephoneInbound } from "react-icons/bs";
-import { CgProfile } from "react-icons/cg";
-// import useShoppingStore from "@/store/shoppingStore";
-import { useState, useEffect } from "react";
+import { VscArchive } from "react-icons/vsc";
 
 const Footer = () => {
-  // const { cart } = useShoppingStore();
-
-  const [cartItemsCount, setCartItemsCount] = useState(0);
-  // useEffect(() => {
-  //   if (cart && cart.cartItems) {
-  //     setCartItemsCount(
-  //       cart.cartItems.reduce((acc, cur) => acc + cur.quantity, 0)
-  //     );
-  //   }
-  // }, [cart]);
   const directionIcons = [
     {
-      title: "حساب کاربری",
+      title: "محصولات",
       linkTitle: "/login",
-      icon: <CgProfile size={24} />,
+      icon: <VscArchive size={24} />,
     },
 
     {
@@ -39,7 +26,7 @@ const Footer = () => {
   ];
   const links = [
     {
-      title: "مجموعه تتیسان",
+      title: "مجموعه اعجاز شیمی",
       items: ["درباره ما", "تماس با ما", "قوانین و مقررات"],
     },
     {
@@ -76,7 +63,7 @@ const Footer = () => {
                   <div className="flex items-center">{item.icon}</div>
                 </div>
                 <div className="my-1">{item.title}</div>
-                <div className="absolute  inset-x-0 bottom-0 h-0.5 bg-pedram-2 w-full transform scale-x-0 group-hover:scale-x-100 transition-transform"></div>
+                <div className="absolute  inset-x-0 bottom-0 h-0.5 bg-pedram-3 w-full transform scale-x-0 group-hover:scale-x-100 transition-transform"></div>
               </div>
             </Link>
           ))}
@@ -84,10 +71,10 @@ const Footer = () => {
       </div>
       <div className="border-t border-pedram-5"></div>
       <div className=" flex justify-between items-center p-4 text-center text-pedram-1">
-        <div className="text-pedram-1  text-4xl font-bold ">
-          <Link href="/">TETISAN </Link>
+        <div className="text-pedram-3  text-4xl font-bold ">
+          <Link href="/">Rain.G</Link>
         </div>
-        <p>&copy; 2023 TETISAN</p>
+        <p>&copy; 2023 Rain.G</p>
       </div>
     </footer>
   );
--- src/components/Main/Navbar.js
+++ src/components/Main/Navbar.js
@@ -1,22 +1,11 @@
 "use client";
 import Link from "next/link";
-import { FaCartShopping } from "react-icons/fa6";
 import { MdOutlinePeopleOutline } from "react-icons/md";
 import { BsTelephoneInbound } from "react-icons/bs";
-import { CgProfile } from "react-icons/cg";
-import useShoppingStore from "@/store/shoppingStore";
-import { useState, useEffect } from "react";
+import { VscArchive } from "react-icons/vsc";
+
 
 const Navbar = () => {
-  // const { cart } = useShoppingStore();
-  const [cartItemsCount, setCartItemsCount] = useState(0);
-  // useEffect(() => {
-  //   if (cart && cart.cartItems) {
-  //     setCartItemsCount(
-  //       cart.cartItems.reduce((acc, cur) => acc + cur.quantity, 0)
-  //     );
-  //   }
-  // }, [cart]);
 
   const lists = [
     {
@@ -30,14 +19,14 @@ const Navbar = () => {
       icon: <BsTelephoneInbound size={24} />,
     },
     {
-      title: "حساب کاربری",
+      title: "محصولات",
       linkTitle: "/login",
-      icon: <CgProfile size={24} />,
+      icon: <VscArchive  size={24} />,
     },
   ];
   return (
-    <nav className="flex items-center justify-between   bg-pedram-13 p-4 fixed top-0 w-full z-10">
-        <div className="  w-1/5 text-pedram-1  text-4xl font-bold">
+    <nav className="hidden md:flex items-center justify-between   bg-pedram-4 p-4 fixed top-0 w-full z-10">
+        <div className=" w-1/5 text-pedram-3  text-4xl font-bold">
           <Link href="/">Rain.G</Link>
         </div>
       <div className="flex items-center mx-4">
@@ -46,7 +35,7 @@ const Navbar = () => {
             <div className="flex mx-4 items-center text-gray-700 text-l relative group">
               <span className="mx-2">{item.icon}</span>
               <span className="mx-2 pb-2">{item.title}</span>
-              <div className="absolute  inset-x-0 bottom-0 h-0.5 bg-pedram-2 w-full transform scale-x-0 group-hover:scale-x-100 transition-transform"></div>
+              <div className="absolute  inset-x-0 bottom-0 h-0.5 bg-pedram-3 w-full transform scale-x-0 group-hover:scale-x-100 transition-transform"></div>
             </div>
           </Link>
         ))}
